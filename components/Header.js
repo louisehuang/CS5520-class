fb0e1489e3d7e92b533ae4d334da8033254e98c4
louisehuang <louisehuang55555@gmail.com>
2024-02-02 23:32:36 -0800

    week 4

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function Header({ name }) {
+  return (
+    <View>
+      <Text style={styles.header}>Welcome to {name}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    color: "darkmagenta",
+    fontSize: 20,
+    borderColor: "darkmagenta",
+    borderWidth: 2,
+    padding: 5,
+    borderRadius: 5,
+  },
+});
---
+++ components/Input.js
@@ -0,0 +1,78 @@
+import {
+  Button,
+  Image,
+  Modal,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+export default function Input({ inputHandler, modalVisible, dismissModal }) {
+  const [text, setText] = useState("");
+  // callback handler
+  function changeTextHandler(changedText) {
+    // console.log("user is typing ", changedText);
+
+    setText(changedText);
+  }
+
+  function confirmHandler() {
+    inputHandler(text);
+    setText("");
+  }
+  function cancelHandler() {
+    setText("");
+
+    // hide the modal
+    dismissModal();
+  }
+  return (
+    <Modal visible={modalVisible}>
+      <View style={styles.container}>
+        <Image
+          source={{
+            uri: "https://cdn-icons-png.flaticon.com/512/2617/2617812.png",
+          }}
+          style={styles.image}
+        />
+        <Image source={require("../assets/goal.png")} style={styles.image} />
+        <TextInput
+          placeholder="Type something"
+          style={styles.input}
+          value={text}
+          onChangeText={changeTextHandler}
+        />
+        <View style={styles.buttonsContainer}>
+          <View style={styles.buttonView}>
+            <Button title="Cancel" onPress={cancelHandler} />
+          </View>
+          <View style={styles.buttonView}>
+            <Button title="Confirm" onPress={confirmHandler} disabled={!text} />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonView: {
+    width: "30%",
+    margin: 5,
+  },
+  buttonsContainer: { flexDirection: "row" },
+  input: {
+    borderBottomWidth: 2,
+    borderBottomColor: "purple",
+    width: "50%",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#999",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  image: { width: 100, height: 100 },
+});
